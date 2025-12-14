4d931207b9e02d4b994a51f795462bb7c7a0bee9
GabrielS <1gabrielsantos3007@gmail.com>
2025-12-14 03:10:33 -0300

    Ajuste em index.html especificamente no js da página para prevenir bug no momento de navegação entre as páginas.

--- js/topico.js
+++ js/topico.js
@@ -1,14 +1,19 @@
+// 1. A ARMADILHA: Empurra um estado novo para o histórico assim que carrega.
+history.pushState(null, null, window.location.href);
+
+// 2. O EVENTO: Agora, quando clicar em voltar, o evento popstate VAI disparar
+window.addEventListener('popstate', (event) => {
+    // Redireciona forçadamente para o index
+    window.location.href = '../index.html'; 
+});
+
 function selectTheme(theme) {
-  localStorage.setItem('selectedTheme', theme);
-  const folderPath = `questions_${theme}/`;
-  const fileName = `${theme}.html`;
-  window.location.href = folderPath + fileName;
+    localStorage.setItem('selectedTheme', theme);
+    const folderPath = `questions_${theme}/`;
+    const fileName = `${theme}.html`;
+    window.location.href = folderPath + fileName;
 }
 
 function navigateTo(page) {
-  window.location.href = page;
+    window.location.href = page;
 }
-
-window.addEventListener('popstate', (event) => {
-  window.location.href = '../index.html';
-});
